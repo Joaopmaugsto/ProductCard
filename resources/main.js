6834bf233403273b343c5fcbeb6e7200b65a44fa
João Pedro Martins Augusto <96791119+Joaopmaugsto@users.noreply.github.com>
2022-07-13 20:02:24 +0100

    Add files via upload

---
+++ resources/main.js
@@ -0,0 +1,11 @@
+// Function to change image when hover with mouse
+
+function hover(element) {
+    element.setAttribute('src', './resources/img/rolex-full.jpg');
+}
+  
+function unhover(element) {
+    element.setAttribute('src', './resources/img/rolex.jpg');
+}
+
+
